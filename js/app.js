aa8f6385d451508980397db8ae25979ae3925368
MATTHEW DIGISO <126MDIGISO@FRHSDCOM>
2024-11-21 13:41:40 -0500

    commit and push final chnages

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var books= document.querySelecgorAll('#book-list li .name');
+const list=document.querySelector('#book-list ul');
 
-Array.from(books).forEach(fucntion(book){
-    book.textContent+= ' (book title)';
-}):
 
-const bookList = document.querySelector('#book-list');
-// console.log(bookList.innerHTML)
-bookList.innerHTML += '<p>This is how you add HTML</p>';
+list.addEventListener('click'.function(e){
+    if(e.target.className=='delete'){
+        const li=e.target.parentElement;
+        list.removeChild(li);
+    }
+});
